398a2f67366c414c813b65ec1bed97cbdb847b83
Mathieu SAMARIA <mathieusamaria974@gmail.com>
2022-07-30 22:00:17 +0400

    integration du header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,28 @@
+<div class="border-b-2 border-zinc-400 border-opacity-10">
+    <div class="relative flex items-center justify-between h-28">
+        <img class="ml-20" src="{{ Storage::url('Foodieland.png') }}" alt="Logo-Foodieland">
+
+        <ul class="flex space-x-20 font-['Inter'] font-medium text-xl">
+
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Recipes</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About us</a></li>
+
+        </ul>
+        <ul class="flex space-x-12 mr-12 ">
+
+            <li>
+                <img src="{{ Storage::url('facebook.png') }}" alt="Facebook">
+            </li>
+            <li>
+                <img src="{{ Storage::url('twitter.png') }}" alt="Twitter">
+            </li>
+            <li>
+                <img src="{{ Storage::url('instagram.png') }}" alt="Instagram">
+            </li>
+
+        </ul>
+    </div>
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <x-head/>
+</head>
+
+<body>
+
+    <header>
+       <x-header/>
+    </header>
+
+    <main>
+        
+    </main>
+
+
+    <footer class="text-gray-600 body-font">
+        
+    </footer>
+
+</body>
+
+</html>
